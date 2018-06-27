0f5ac9a1268ac8a16cc3e35d161adb5e4153e7ee
Leandro Grimaldi <leandro.grimaldi@sulamerica.com.br>
2018-06-27 10:38:26 -0300

    Commit inicial Nodejs MongoDB

---
+++ movie-service/src/api/movies.js
@@ -0,0 +1,30 @@
+module.exports = (app, repository) => {
+
+    app.get('/', (req, res, next) => {
+        repository.getAllMovies((err, movies) => {
+          if(err) return next(err);
+          res.write('Ok!');
+        });
+      })
+
+    app.get('/movies', (req, res, next) => {
+      repository.getAllMovies((err, movies) => {
+        if(err) return next(err);
+        res.json(movies);
+      });
+    })
+  
+    app.get('/movies/premieres', (req, res, next) => {
+      repository.getMoviePremiers((err, movies) => {
+        if(err) return next(err);
+        res.json(movies)
+      });
+    })
+  
+    app.get('/movies/:id', (req, res, next) => {
+      repository.getMovieById(req.params.id, (err, movie) => {
+        if(err) return next(err);
+        res.json(movie)
+      });
+    })
+  }

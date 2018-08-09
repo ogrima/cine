46d75025328df8bb2a7aa776ee22f28865d85563
Leandro Grimaldi <lbgrimaldi@gmail.com>
2018-08-08 23:15:03 -0300

    Concluindo Servico.

--- cinema-catalog-service/src/repository/repository.js
+++ cinema-catalog-service/src/repository/repository.js
@@ -18,8 +18,75 @@ function getCinemasByCityId(cityId, callback){
     });
 }
 
+function getMoviesByCinemaId(cinemaId, callback){
+    var objCinemaId = ObjectId(cinemaId);
+    mongodb.connect((err, db) => {
+        db.collection("cinemaCatalog").aggregate([
+            {$match: {"cinemas._id": objCinemaId}},
+            {$unwind: "$cinemas"},
+            {$unwind: "$cinemas.salas"},
+            {$unwind: "$cinemas.salas.sessoes"},
+            {$group: {_id: { filme: "$cinemas.salas.sessoes.filme", idFilme: "$cinemas.salas.sessoes.idFilme"}}}
+        ]).toArray(callback);
+    });
+}
+
+function getMoviesByCityId(cityId, callback){
+    var objCityId = ObjectId(cityId);
+    mongodb.connect((err, db) => {
+        db.collection("cinemaCatalog").aggregate([
+            {$match: {"_id": objCityId}},
+            {$unwind: "$cinemas"},
+            {$unwind: "$cinemas.salas"},
+            {$unwind: "$cinemas.salas.sessoes"},
+            {$group: {_id: { filme: "$cinemas.salas.sessoes.filme", idFilme: "$cinemas.salas.sessoes.idFilme"}}}
+        ]).toArray((err, sessions) => {
+            if(err) return callback(err, null);
+            callback(err, sessions.map(item => { return {idFilme: item._id.idFilme, filme: item._id.filme } } ));
+        });
+    });
+}
+
+function getMovieSessionsByCityId(movieId, cityId, callback){
+    var objMovieId = ObjectId(movieId);
+    var objCityId = ObjectId(cityId);
+
+    mongodb.connect((err, db) => {
+        db.collection("cinemaCatalog").aggregate([
+            {$match: {"_id": objCityId}},
+            {$unwind: "$cinemas"},
+            {$unwind: "$cinemas.salas"},
+            {$unwind: "$cinemas.salas.sessoes"},
+            {$match: {"cinemas.salas.sessoes.idFilme": objMovieId}},
+            {$group: {_id: { filme: "$cinemas.salas.sessoes.filme", idFilme: "$cinemas.salas.sessoes.idFilme", idCinema: "$cinemas._id", sala: "$cinemas.salas.nome", sessao: "$cinemas.salas.sessoes"}}}
+        ]).toArray((err, sessions) => {
+            if(err) return callback(err, null);
+            callback(err, sessions.map(item => { return {idFilme: item._id.idFilme, filme: item._id.filme, idCinema: item._id.idCinema, sala: item._id.sala, sessao: item._id.sessao } } ));
+        });
+    });
+}
+
+function getMovieSessionsByCinemaId(movieId, cinemaId, callback){
+    var objCinemaId = ObjectId(cinemaId);
+    var objMovieId = ObjectId(movieId);
+    mongodb.connect((err, db) => {
+        db.collection("cinemaCatalog").aggregate([
+            {$match: {"cinemas._id": objCinemaId}},
+            {$unwind: "$cinemas"},
+            {$unwind: "$cinemas.salas"},
+            {$unwind: "$cinemas.salas.sessoes"},
+            {$match: {"cinemas.salas.sessoes.idFilme": objMovieId}},
+            {$group: {_id: { filme: "$cinemas.salas.sessoes.filme", idFilme: "$cinemas.salas.sessoes.idFilme", sala: "$cinemas.salas.nome", sessao: "$cinemas.salas.sessoes"}}}
+        ]).toArray((err, sessions) => {
+            if(err) return callback(err, null);
+            callback(err, sessions.map(item => { return {idFilme: item._id.idFilme, filme: item._id.filme, sala: item._id.sala, sessao: item._id.sessao } } ));
+        });
+    });
+}
+
 function disconnect(){
     return mongodb.disconnect();
 }
 
-module.exports = { getAllCities, getCinemasByCityId, disconnect }
+	
+module.exports = { getAllCities, getCinemasByCityId, getMoviesByCityId, getMoviesByCinemaId, getMovieSessionsByCityId, getMovieSessionsByCinemaId, disconnect }
--- cinema-catalog-service/src/repository/repository.test.js
+++ cinema-catalog-service/src/repository/repository.test.js
@@ -25,10 +25,40 @@ function runTests(){
         });
     })
 
+    test('Repository getMoviesByCinemaId', (t) => {
+        repository.getMoviesByCinemaId(cinemaId, (err, movies) => {
+            t.assert(!err && movies && movies.length > 0, "Movies By Cinema Id Returned");
+            t.end();
+        });
+    })
+
+    test('Repository getMoviesByCityId', (t) => {
+        repository.getMoviesByCityId(cityId, (err, movies) => {
+            if(movies && movies.length > 0) movieId = movies[1].idFilme;//Era de Ultron
+            t.assert(!err && movies && movies.length > 0, "Movies By City Id Returned");
+            t.end();
+        });
+    })
+
+    test('Repository getMovieSessionsByCityId', (t) => {
+        repository.getMovieSessionsByCityId(movieId, cityId, (err, sessions) => {
+            t.assert(!err && sessions && sessions.length > 0, "Movie Sessions By City Id Returned");
+            t.end();
+        });
+    })
+
+    test('Repository getMovieSessionsByCinemaId', (t) => {
+        repository.getMovieSessionsByCinemaId(movieId, cinemaId, (err, sessions) => {
+            t.assert(!err && sessions && sessions.length > 0, "Movie Sessions By Cinema Id Returned");
+            t.end();
+        });
+    })
+
     test('Repository Disconnect', (t) => {
         t.assert(repository.disconnect(), "Disconnect Ok");
         t.end();
     })
 }
+
 
 module.exports = { runTests }
